68f736cc27e0a57481b91c194094b906da7e0bb2
abiola1864 <oyebanjoabiola@gmail.com>
2023-07-04 00:06:33 +0200

    spotlightesting

---
+++ components/SpotLight.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Spotlight from "../components/Spotlight";
+
+test("renders the wrapper div and artist correctly", () => {
+  const artist = "John Doe";
+
+  const { getByText } = render(<Spotlight artist={artist} />);
+
+
+// parentNode property is used to retrieve
+// the parent element of the element
+//  containing the artist's name
+
+  const wrapperElement = getByText(artist).parentNode;
+  const artistElement = getByText(artist);
+
+  expect(wrapperElement).toBeInTheDocument();
+  expect(artistElement).toBeInTheDocument();
+});
